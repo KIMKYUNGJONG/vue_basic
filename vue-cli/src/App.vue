f712d7c3535cf371a046b98cb22aa0e9f8098a5f
KIMKUNGJONG <37282516+KIMKUNGJONG@users.noreply.github.com>
2021-09-19 04:38:06 +0900

    2021-09-19 git basic study

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <app-header 
+      v-bind:data="str"
+      v-on:renew="renewStr"
+    ></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data: function() {
+    return{
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function(){
+      this.str = 'Renew'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
